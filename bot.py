01f5a706fb92f84409cb8d80f34184b46dc2a76f
Azamat8606 <131238729+Azamat8606@users.noreply.github.com>
2023-04-19 14:59:13 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,21 +1,24 @@
+import time
 import telebot
 import bs4
 from Task import Task
 import parser_1
 import markups as m
+from aiogram import Bot, Dispatcher, executor, types
 
 #main variables
-
-bot = telebot.TeleBot(TOKEN)
+TOKEN = "6282212419:AAFGxLS3vxlscXvh1CFNaYstR7ZmuzYiJP0"
+bot = Bot(token=TOKEN)
 task = Task()
+dp = Dispatcher(bot=bot)
 
 #handlers
-@bot.message_handler(commands=['start', 'go'])
+@dp.message_handler(commands=['start', 'go'])
 def start_handler(message):
     if not task.isRunning:
         chat_id = message.chat.id
-        msg = bot.send_message(chat_id, 'Откуда парсить?', reply_markup=m.source_markup)
-        bot.register_next_step_handler(msg, askSource)
+        msg = dp.send_message(chat_id, 'Откуда парсить?', reply_markup=m.source_markup)
+        dp.register_next_step_handler(msg, askSource)
         task.isRunning = True
 
 def askSource(message):
@@ -23,15 +26,15 @@ def askSource(message):
     text = message.text.lower()
     if text in task.names[0]:
         task.mySource = 'top'
-        msg = bot.send_message(chat_id, 'За какой временной промежуток?', reply_markup=m.age_markup)
-        bot.register_next_step_handler(msg, askAge)
+        msg = dp.send_message(chat_id, 'За какой временной промежуток?', reply_markup=m.age_markup)
+        dp.register_next_step_handler(msg, askAge)
     elif text in task.names[1]:
         task.mySource = 'all'
-        msg = bot.send_message(chat_id, 'Какой минимальный порог рейтинга?', reply_markup=m.rating_markup)
-        bot.register_next_step_handler(msg, askRating)
+        msg = dp.send_message(chat_id, 'Какой минимальный порог рейтинга?', reply_markup=m.rating_markup)
+        dp.register_next_step_handler(msg, askRating)
     else:
-        msg = bot.send_message(chat_id, 'Такого раздела нет. Введите раздел корректно.')
-        bot.register_next_step_handler(msg, askSource)
+        msg = dp.send_message(chat_id, 'Такого раздела нет. Введите раздел корректно.')
+        dp.register_next_step_handler(msg, askSource)
         return
 
 def askAge(message):
@@ -39,35 +42,35 @@ def askAge(message):
     text = message.text.lower()
     filters = task.filters[0]
     if text not in filters:
-        msg = bot.send_message(chat_id, 'Такого временного промежутка нет. Введите порог корректно.')
-        bot.register_next_step_handler(msg, askAge)
+        msg = dp.send_message(chat_id, 'Такого временного промежутка нет. Введите порог корректно.')
+        dp.register_next_step_handler(msg, askAge)
         return
     task.myFilter = task.filters_code_names[0][filters.index(text)]
-    msg = bot.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
-    bot.register_next_step_handler(msg, askAmount)
+    msg = dp.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
+    dp.register_next_step_handler(msg, askAmount)
 
 def askRating(message):
     chat_id = message.chat.id
     text = message.text.lower()
     filters = task.filters[1]
     if text not in filters:
-        msg = bot.send_message(chat_id, 'Такого порога нет. Введите порог корректно.')
-        bot.register_next_step_handler(msg, askRating)
+        msg = dp.send_message(chat_id, 'Такого порога нет. Введите порог корректно.')
+        dp.register_next_step_handler(msg, askRating)
         return
     task.myFilter = task.filters_code_names[1][filters.index(text)]
-    msg = bot.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
-    bot.register_next_step_handler(msg, askAmount)
+    msg = dp.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
+    dp.register_next_step_handler(msg, askAmount)
 
 def askAmount(message):
     chat_id = message.chat.id
     text = message.text.lower()
     if not text.isdigit():
-        msg = bot.send_message(chat_id, 'Количество страниц должно быть числом. Введите корректно.')
-        bot.register_next_step_handler(msg, askAmount)
+        msg = dp.send_message(chat_id, 'Количество страниц должно быть числом. Введите корректно.')
+        dp.register_next_step_handler(msg, askAmount)
         return
     if int(text) < 1 or int(text) > 5:
-        msg = bot.send_message(chat_id, 'Количество страниц должно быть >0 и <6. Введите корректно.')
-        bot.register_next_step_handler(msg, askAmount)
+        msg = dp.send_message(chat_id, 'Количество страниц должно быть >0 и <6. Введите корректно.')
+        dp.register_next_step_handler(msg, askAmount)
         return
     task.isRunning = False
     print(task.mySource + " | " + task.myFilter + ' | ' + text) #
@@ -76,6 +79,8 @@ def askAmount(message):
         output = parser_1.getTitlesFromTop(int(text), task.myFilter)
     else:
         output = parser_1.getTitlesFromAll(int(text), task.myFilter)
-    msg = bot.send_message(chat_id, output, reply_markup=m.start_markup)
+    msg = dp.send_message(chat_id, output, reply_markup=m.start_markup)
 
-bot.polling(none_stop=True)
+
+if __name__=='__name__' :
+    executor.start_polling(dp)
--- markups.py
+++ markups.py
@@ -1,4 +1,4 @@
-from telebot import types
+from aiogram import types
 
 start_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
 start_markup_btn1 = types.KeyboardButton('/start')
